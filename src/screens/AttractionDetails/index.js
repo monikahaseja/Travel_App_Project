530739fdca900d1ad7118329007fbfb45469758f
Monika Haseja <monikahaseja@gmail.com>
2025-11-10 18:17:25 +0530

    Add location, time, city, title, and price in text section of AttractionDetails Page with releted styles

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, ImageBackground, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Title from '../../components/Title';
+import InfoCard from '../../components/InfoCard';
 import styles from './styles';
-
-
-const ALL = 'All';
 
 const AttractionDetails = ({navigation, route}) => {
     const {item} = route?.params || {};
+
+    const mainImage = item?.images?.length ? item?.images[0] : null;
+    const slicedImages = item?.images?.length ? item?.images?.slice(0, 5) : [];
+    const diffImages = item?.images?.length - slicedImages?.length;
+
+    const onGalleryNavigate = () => {
+        navigation.navigate('Gallery', { images: item?.images });
+    };
 
     const onBack = () => {
         navigation.goBack();
@@ -16,8 +23,49 @@ const AttractionDetails = ({navigation, route}) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text onPress={onBack} style={{margin: 32}}>BACK</Text>
-            <Text>{item?.name}</Text>
+            <ImageBackground 
+                style={styles.mainImage} 
+                imageStyle={{borderRadius: 20}} 
+                source={{uri: mainImage}}
+            >
+                <View style={styles.header}>
+                    <Pressable onPress={onBack} hitSlop={8}>
+                        <Image style={styles.icon} source={require('../../assets/back.png')} />
+                    </Pressable>
+                    <Pressable hitSlop={8}>
+                        <Image style={styles.icon} source={require('../../assets/share.png')} />
+                    </Pressable>
+                </View>
+
+                <Pressable onPress={onGalleryNavigate} style={styles.footer}>
+                    {slicedImages?.map((image, index) => (
+                        <View key={image}>
+                            <Image source={{ uri: image }} style={styles.miniImage} />
+                            {diffImages > 0 && index === slicedImages?.length - 1 ? (
+                                <View style={styles.moreImagesContainer}>
+                                    <Text style={styles.moreImages}>{`+${diffImages}`}</Text>
+                                </View>
+                            ) : null}
+                        </View>
+                    ))}
+                </Pressable>
+
+            </ImageBackground>
+            
+            <View style={styles.headerContainer}>
+                <View>
+                    <Title style={styles.title} text={item?.name} />
+                    <Text style={styles.city}>{item?.city}</Text>
+                </View>
+                <Title style={styles.title} text={item?.entry_price} />
+            </View>
+
+            <InfoCard text={item?.address} icon={require('../../assets/location_circle.png')} />
+            <InfoCard
+                text={`OPEN ${item?.opening_time} - ${item?.closing_time}`}
+                icon={require('../../assets/schedule.png')}
+            />
+
         </SafeAreaView>
     );
 };
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -1,25 +1,77 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        margin: 32,
     },
-    subtitle: {
-        fontSize: 20,
-        color: '#000000',
-        marginTop: 40,
+    mainImage: {
+        width: '100%',
+        height: height / 2,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
-    row: {
+    header: {
+        width: '100%',
         flexDirection: 'row',
-        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-    emptyText: {
-        textAlign: 'center',
-        marginTop: 40,
-        fontSize: 20,
-        color: 'rgba(0,0,0,0.5)',
+    icon: {
+        width: 36,
+        height: 36,
+        margin: 16,
+    },
+    footer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15,
+        backgroundColor: 'rgba(256, 256, 256, 0.35)',
+        margin: 16,
+        paddingHorizontal: 8,
+    },
+    miniImage: {
+        width: 40,
+        height: 40,
+        marginHorizontal: 4,
+        marginVertical: 8,
+        borderRadius: 10,
+    },
+    moreImages: {
+        color: '#fff',
         fontWeight: 'bold',
-    }
+        fontSize: 20,
+    },
+    moreImagesContainer: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,0.38)',
+        width: 40,
+        height: 40,
+        top: 8,
+        left: 4,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 40,
+    },
+    title: {
+        color: '#000',
+        fontSize: 25,
+    },
+    city: {
+        fontSize: 20,
+        color: '#000',
+        fontWeight: '400',
+        marginTop: 8,
+    },
 });
 
 export default styles;
