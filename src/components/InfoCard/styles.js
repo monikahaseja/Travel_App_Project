dbe675c70a4720f4869d248f5347f2b61a60bc54
Monika Haseja <monikahaseja@gmail.com>
2025-11-10 18:19:28 +0530

    Add InfoCard Components for display text section of AttractionDetails page

---
+++ src/components/InfoCard/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16
+    },
+    icon: {
+        width: 40,
+        height: 40,
+    },
+    title: {
+        fontSize: 15,
+        color: '#000',
+        marginHorizontal: 8,
+        fontWeight: 'bold',
+        marginRight: 32,
+    }
+});
+
+export default styles;
